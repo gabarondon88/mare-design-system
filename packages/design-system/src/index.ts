b9b636b8b238617cc36a965bc2d22d9f560588d1
gabarondon88 <gaba.rondon88@gmail.com>
2026-08-10 09:01:16 +0200

    Export Tabs component API

--- packages/design-system/src/index.ts
+++ packages/design-system/src/index.ts
@@ -2,3 +2,5 @@ export { Button, buttonContract, buttonDefaults } from "./button";
 export type { ButtonProps } from "./button";
 export { MareProvider } from "./mare-provider";
 export type { MareProviderProps } from "./mare-provider";
+export { Tab, TabList, TabPanel, Tabs, tabsContract } from "./tabs";
+export type { TabListProps, TabPanelProps, TabProps, TabsProps } from "./tabs";
