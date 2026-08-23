ad4757867dc08f85d0d4d49d8959225ffece680c
gabarondon88 <gaba.rondon88@gmail.com>
2026-08-23 17:56:35 +0200

    Export Input component API

--- packages/design-system/src/index.ts
+++ packages/design-system/src/index.ts
@@ -2,5 +2,7 @@ export { Button, buttonContract, buttonDefaults } from "./button";
 export type { ButtonProps } from "./button";
 export { MareProvider } from "./mare-provider";
 export type { MareProviderProps } from "./mare-provider";
+export { Input, inputContract, inputDefaults } from "./input";
+export type { InputProps, InputVisualState } from "./input";
 export { Tab, TabList, TabPanel, Tabs, tabsContract } from "./tabs";
 export type { TabListProps, TabPanelProps, TabProps, TabsProps } from "./tabs";
